440b41c55e0ec9683b816181a666778360316c9e
Dani Renner <danijrenner@gmail.com>
2021-03-08 16:15:14 -0800

    add methods with database
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

--- Packer/Models/Item.cs
+++ Packer/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace Packer.Models
 {
@@ -9,37 +10,77 @@ namespace Packer.Models
     public bool Purchased { get; set; }
     public int Id { get; set;}
 
-    private static List<Item> _instances = new List<Item>{};
-
     public Item(string name, bool packed, bool purchased)
     {
       Name = name;
       Packed = packed;
       Purchased = purchased;
-      _instances.Add(this);
-      Id = _instances.Count;
+    }
+    public Item(string name, bool packed, bool purchased, int id)
+    {
+      Name = name;
+      Packed = packed;
+      Purchased = purchased;
+      Id = id;
     }
 
     public static List<Item> GetAll()
     {
-      return _instances;
+      List<Item> allItems = new List<Item> { };
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while (rdr.Read())
+      {
+          int itemId = rdr.GetInt32(0);
+          string itemName = rdr.GetString(1);
+          bool itemPacked = rdr.GetBoolean(2);
+          bool itemPurchased = rdr.GetBoolean(3);
+          Item newItem = new Item(itemName, itemPacked, itemPurchased, itemId);
+          allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      return allItems;
     }
     public static void ClearAll()
     {
-      _instances.Clear();
+    
     }
 
     public static Item Find(int searchId)
     {
-      return _instances[searchId-1];
+      Item itemName = new Item("socks", true, true);
+      return itemName;
     }
 
     public static void Edit(int id, string name, bool packed, bool purchased)
     {      
-      _instances[id-1].Name = name;
-      _instances[id-1].Packed = packed;
-      _instances[id-1].Purchased = purchased;
+    }
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+
+      cmd.CommandText = @"INSERT INTO items (name) VALUES (@ItemName);";
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@ItemName";
+      name.Value = this.Name;
+      cmd.Parameters.Add(name);    
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
     }
   }
-
 }
